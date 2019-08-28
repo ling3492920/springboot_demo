a3306f7d78ba6c187408f24424db2cb71b5219fc
tianqueyf <18721658364@163.com>
2019-08-28 16:39:04 +0800

    init proj

---
+++ src/main/java/cn/jhd/springboot_demo/hello/HelloController.java
@@ -0,0 +1,19 @@
+package cn.jhd.springboot_demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author jianghd
+ * @create 2019/8/28-16:12
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
